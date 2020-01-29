fb908914718dda7859881631d83d93a9b033a5ab
aperezmm <alejandro.perez@udea.edu.co>
2020-01-28 23:33:19 -0500

    Formularios basicos

--- src/app/coches/coches.component.ts
+++ src/app/coches/coches.component.ts
@@ -9,16 +9,22 @@ import { Coche } from './coche';
 export class CochesComponent implements OnInit {
 
   public coche: Coche;
+  public coches:Array<Coche>
 
   constructor() { 
     this.coche = new Coche(" "," "," "," ");
+    this.coches = [
+      new Coche("Renault", "Clio", "107", "Gris perla"),
+      new Coche("Renault", "Logan", "86", "Negro satin")
+    ];
   }
 
   ngOnInit() {
   }
 
   onSubmit(){
-    console.log(this.coche);
+    this.coches.push(this.coche);
+    this.coche = new Coche(" "," "," "," ");
   }
 
 }
